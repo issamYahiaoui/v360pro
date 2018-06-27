3ea62235f76828cd9f1b1ea6a2acd1feb1267a2d
issamYahiaoui <ei_yahiaoui@esi.dz>
2018-06-27 13:19:33 +0100

    first commit  ...

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'phone' => ['required'],
+            'password' => ['required']
+        ]);
+
+
+        // Set up the login attempt
+        $credentials = [
+            'phone' => $request->input('phone'),
+            'password' => $request->input('password'),
+
+        ];
+
+        // Attempt to auth the user
+        if (Auth::attempt($credentials)) {
+            // Login success
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+
+    protected function authenticated(Request $request, $user)
+    {
+        return redirect($this->redirectTo);
+    }
+}
