81b7b5e85911c283e8b41554c9191c23c3ff7746
issamYahiaoui <ei_yahiaoui@esi.dz>
2018-06-27 18:30:10 +0100

    update  ...

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/tours';
 
     /**
      * Create a new controller instance.
